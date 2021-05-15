671bcd4c48863c2f25b5a2199ab226327bb9de5f
Kolby McDaniel <kolbymcdaniel@gmail.com>
2021-05-15 16:28:14 -0400

    updated2

--- ColorShop/CSModels/Cart.cs
+++ ColorShop/CSModels/Cart.cs
@@ -4,5 +4,14 @@ namespace CSModels
     {
         public Cart() {}
         public Cart(Product product, int quantity, double amount)
+        {
+            this.Product = product;
+            this.Quantity = quantity;
+            this.Amount = amount;
+        }
+
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public double Amount { get; set; }
     }
 }
